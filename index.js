4669529e4b207f52a54ebe1ce0985f3d19491088
Matthew Freeman <matthew.freeman821@gmail.com>
2018-08-12 19:49:25 -0400

    Updated to render hello in the browser

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const BetMe = require('./db');
 const express = require('express');
+const expressHbs = require('express-handlebars');
 const app = express();
+
+// This shit is necessary for the app to work and render something in the browser, all the way through the next comment
+app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+const static = express.static;
+app.use(static('public'));
+app.use(static('public/images'));
+// This is the end of necessary shit for this to work and render things in the browser
+
+console.log('This should work');
+
+app.get('/', (req, res) => {
+    res.render('homepage')
+});
+
+
+
+
+
 app.listen(3000, () => {
-    console.log('your server is running!');
+    console.log('your server is running on port 3000!');
 });
 
 console.log('Server is here');
